c40105d55117e086ebf573f98678083464d6562d
Arni Magnusson <thisisarni@gmail.com>
2026-05-23 03:10:06 +1100

    Calculate metrics, identify empty analyses

--- model.R
+++ model.R
@@ -1,7 +1,7 @@
 # Run analysis, write model results
 
-# Before: checks.csv (data)
-# After:  no.rds (model)
+# Before: checks.csv, files.csv (data)
+# After:  checks.csv, empty.csv, metrics.csv, no.rds, overview.csv (model)
 
 library(TAF)
 
@@ -9,21 +9,21 @@ mkdir("model")
 
 # Read QC checks
 checks <- read.taf("data/checks.csv")
+files <- read.taf("data/files.csv")
 
-# Count number of analyses passing checks
-overview <- colSums(taf2xtab(checks))
-overview <- data.frame(Check=names(overview), Yes=overview,
-                       No=nrow(checks)-overview, row.names=NULL)
-
-# Remove rows that are not relevant for ices-advice
+# Remove checks that are not relevant for ices-advice
 not.relevant <- c("dir.exists",                 # all dirs exist
                   "qc.data.bib.processed",      # DATA.bib not processed
                   "qc.data.declared",           # no boot/data folder
                   "qc.software.bib.processed",  # SOFTWARE.bib not processed
                   "qc.software.declared",       # no boot/software folder
                   "qc.initial.data")            # no boot/data folder
-overview <- overview[!overview$Check %in% not.relevant,]
-row.names(overview) <- NULL
+checks <- checks[!names(checks) %in% not.relevant]
+
+# Count number of analyses passing checks
+overview <- colSums(taf2xtab(checks))
+overview <- data.frame(Check=names(overview), Yes=overview,
+                       No=nrow(checks)-overview, row.names=NULL)
 
 # List failed checks
 not <- function(check) checks$Analysis[!checks[check]]
@@ -32,8 +32,24 @@ no <- list(boot.exists=not("qc.boot.exists"),
            data.bib.valid=not("qc.data.bib.valid"),
            software.bib.exists=not("qc.software.bib.exists"),
            software.bib.valid=not("qc.software.bib.valid"),
-           scripts.exist=not("qc.scripts.exist"),
+           any.scripts.exist=not("qc.any.scripts.exist"),
+           all.scripts.exist=not("qc.all.scripts.exist"),
            only.relative.paths=not("qc.only.relative.paths"))
 
-# Save failed checks
+# Metrics
+score <- apply(taf2xtab(checks), 1, sum)
+score <- data.frame(Analysis=names(score),
+                    Year=as.integer(substring(names(score), 1, 4)),
+                    Checks=score, row.names=NULL)
+volume <- aggregate(cbind(Bytes, Lines)~Analysis, files, sum)
+metrics <- merge(score, volume)
+
+# Which analyses seem empty
+empty <- metrics[metrics$Bytes < 2000 | metrics$Lines < 40,]
+
+# Write tables and lists
+write.taf(checks, dir="model")
+write.taf(empty, dir="model")
+write.taf(metrics, dir="model")
+write.taf(overview, dir="model")
 saveRDS(no, "model/no.rds")
